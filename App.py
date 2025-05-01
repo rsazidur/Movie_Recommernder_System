f4af794f1ed99b13588cb54c5381edf5e7ae4690
rsazidur <rsazidur936@gmail.com>
2025-05-01 17:31:44 +0600

    Fixed poster Size and Names

--- App.py
+++ App.py
@@ -7,52 +7,47 @@ import requests
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=9136e05c3e819b15ece0ed759be1b3f9&language=en-US'.format(movie_id))
     data = response.json()
-    return  "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
     movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-    
+
     recommended_movies = []
     recommended_movies_posters = []
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
-        # fetch poster from api
-        
+        # fetch poster from API
         recommended_movies.append(movies.iloc[i[0]].title)
         recommended_movies_posters.append(fetch_poster(movie_id))
     return recommended_movies, recommended_movies_posters
 
+
+# Load movie data and similarity matrix
 movies_dict = pickle.load(open('movies_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
 
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
+# Streamlit app layout
 st.title("Movie Recommender System")
 
+# Dropdown for selecting movie
 selected_movie_name = st.selectbox(
     'Type or select a movie name from the dropdown',
     movies['title'].values)
 
 if st.button('Show Recommendation'):
-    names, posters  = recommend(selected_movie_name)
-    
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(names[0])
-        st.image(posters[0])
-    with col2:
-        st.text(names[1])
-        st.image(posters[1])
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
-    
+    names, posters = recommend(selected_movie_name)
+
+    # Creating 5 columns for the recommendation posters and titles
+    cols = st.columns(5)
+    for i in range(5):
+        with cols[i]:
+            st.image(posters[i], use_container_width=True)
+            st.markdown(
+                f"<div style='word-wrap: break-word; text-align:center; font-size:14px; margin-top: 5px'>{names[i]}</div>",
+                unsafe_allow_html=True
+            )
